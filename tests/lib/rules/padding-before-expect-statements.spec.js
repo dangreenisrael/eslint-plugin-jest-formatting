5ce8e3bdcc15875828f054b40895814eecb9bf3f
Ben Kimpel <benkimpel@users.noreply.github.com>
2019-07-02 14:00:10 -0500

    Add expect padding tests

---
+++ tests/lib/rules/padding-before-expect-statements.spec.js
@@ -0,0 +1,137 @@
+/**
+ * @fileoverview Enforces single line padding before expect statements
+ * @author Ben Kimpel
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const { RuleTester } = require('eslint');
+const rule = require('../../../lib').rules['padding-before-expect-statements'];
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const valid = `
+foo();
+bar();
+
+const someText = 'abc';
+const someObject = {
+  one: 1,
+  two: 2,
+};
+
+test('thing one', () => {
+  const abc = 123;
+
+  expect(abc).toEqual(123);
+  expect(123).toEqual(abc); // Line comment
+});
+
+test('thing one', () => {
+  const abc = 123;
+
+  expect(abc).toEqual(123);
+
+  const xyz = 987;
+
+  expect(123).toEqual(abc); // Line comment
+});
+
+describe('someText', () => {
+  describe('some condition', () => {
+    test('foo', () => {
+      const xyz = 987;
+
+      // Comment
+      expect(xyz).toEqual(987);
+      expect(1)
+        .toEqual(1);
+      expect(true).toEqual(true);
+    });
+  });
+});
+`;
+
+const invalid = `
+foo();
+bar();
+
+const someText = 'abc';
+const someObject = {
+  one: 1,
+  two: 2,
+};
+
+test('thing one', () => {
+  const abc = 123;
+  expect(abc).toEqual(123);
+  expect(123).toEqual(abc); // Line comment
+});
+
+test('thing one', () => {
+  const abc = 123;
+  expect(abc).toEqual(123);
+
+  const xyz = 987;
+  expect(123).toEqual(abc); // Line comment
+});
+
+describe('someText', () => {
+  describe('some condition', () => {
+    test('foo', () => {
+      const xyz = 987;
+      // Comment
+      expect(xyz).toEqual(987);
+      expect(1)
+        .toEqual(1);
+      expect(true).toEqual(true);
+    });
+  });
+});
+`;
+
+ruleTester.run('padding-describe-blocks', rule, {
+  valid: [valid],
+  invalid: [
+    {
+      code: invalid,
+      errors: 4,
+      output: valid,
+    },
+    {
+      code: invalid,
+      errors: [
+        {
+          message: 'Expected blank line before this statement.',
+          line: 13,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 19,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 22,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 30,
+          column: 7
+        },
+      ]
+    },
+  ]
+});
