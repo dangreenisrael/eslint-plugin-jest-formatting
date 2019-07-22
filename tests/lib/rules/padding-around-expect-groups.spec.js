8795d00cf970699f9472ad3008caeaa9448b2609
Dan Green-Leipciger <dangreen.tlv@gmail.com>
2019-07-22 19:52:01 -0400

    Add a rule to enforce padding around expect groups.
    As identified in issue #51, Adding padding before expect statements does not take into consideration that there should also be a line of padding following groups of expect statements as well.
    
    before:
    
    ```
    test('thing one', () => {
      let abc = 123;
    
      expect(abc).toEqual(123);
      expect(123).toEqual(abc); // Line comment
      abc = 456;
    
      expect(abc).toEqual(456);
    });
    ```
    
    after:
    
    ```
    test('thing one', () => {
      let abc = 123;
    
      expect(abc).toEqual(123);
      expect(123).toEqual(abc); // Line comment
    
      abc = 456;
    
      expect(abc).toEqual(456);
    });
    ```

---
+++ tests/lib/rules/padding-around-expect-groups.spec.js
@@ -0,0 +1,160 @@
+/**
+ * @fileoverview Enforces single line padding around groups of expect statements
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const { RuleTester } = require('eslint');
+const rule = require('../../../lib').rules['padding-around-expect-groups'];
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const valid = `
+foo();
+bar();
+
+const someText = 'abc';
+const someObject = {
+  one: 1,
+  two: 2,
+};
+
+test('thing one', () => {
+  let abc = 123;
+
+  expect(abc).toEqual(123);
+  expect(123).toEqual(abc); // Line comment
+
+  abc = 456;
+
+  expect(abc).toEqual(456);
+});
+
+test('thing one', () => {
+  const abc = 123;
+
+  expect(abc).toEqual(123);
+
+  const xyz = 987;
+
+  expect(123).toEqual(abc); // Line comment
+});
+
+describe('someText', () => {
+  describe('some condition', () => {
+    test('foo', () => {
+      const xyz = 987;
+
+      // Comment
+      expect(xyz).toEqual(987);
+      expect(1)
+        .toEqual(1);
+      expect(true).toEqual(true);
+    });
+  });
+});
+`;
+
+const invalid = `
+foo();
+bar();
+
+const someText = 'abc';
+const someObject = {
+  one: 1,
+  two: 2,
+};
+
+test('thing one', () => {
+  let abc = 123;
+  expect(abc).toEqual(123);
+  expect(123).toEqual(abc); // Line comment
+  abc = 456;
+  expect(abc).toEqual(456);
+});
+
+test('thing one', () => {
+  const abc = 123;
+  expect(abc).toEqual(123);
+
+  const xyz = 987;
+  expect(123).toEqual(abc); // Line comment
+});
+
+describe('someText', () => {
+  describe('some condition', () => {
+    test('foo', () => {
+      const xyz = 987;
+      // Comment
+      expect(xyz).toEqual(987);
+      expect(1)
+        .toEqual(1);
+      expect(true).toEqual(true);
+    });
+  });
+});
+`;
+
+ruleTester.run('padding-around-expect-groups', rule, {
+  valid: [
+    valid,
+    {
+      code: invalid,
+      filename: 'src/component.jsx',
+    }
+  ],
+  invalid: [
+    {
+      code: invalid,
+      filename: 'src/component.test.jsx',
+      errors: 6,
+      output: valid,
+    },
+    {
+      code: invalid,
+      filename: 'src/component.test.js',
+      errors: [
+        {
+          message: 'Expected blank line before this statement.',
+          line: 13,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 15,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 16,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 21,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 24,
+          column: 3
+        },
+        {
+          message: 'Expected blank line before this statement.',
+          line: 32,
+          column: 7
+        },
+      ]
+    },
+  ]
+});
