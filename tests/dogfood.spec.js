be48f8f8b4a41795c77415e46d4f89eea01dcfa6
Dan Green-Leipciger <dangreen.tlv@gmail.com>
2019-08-05 08:16:39 -0400

    Add some dogfood examples

--- tests/dogfood.spec.js
+++ tests/dogfood.spec.js
@@ -1,15 +1,32 @@
+//  'afterAll',
+
+beforeAll(() => {});
+
+beforeEach(() => {});
+
+afterAll(() => {});
+
+afterEach(() => {});
+
 describe('no padding on top', () => {
   it('has no padding above', () => {
     expect(true);
   });
 
+  expect(true);
+  expect(1);
+
   it('has padding above', () => {
+    expect(true);
+  });
+
+  it('also has padding above but not below', () => {
     expect(true);
   });
 });
 
 describe('padding above', () => {
-  it('has no padding above', () => {
+  it('has no padding above or below', () => {
     expect(true);
   });
 });
