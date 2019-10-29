199f61f34dd10a4f6c754a461b03f187cf202551
Ben Kimpel <benkimpel@users.noreply.github.com>
2019-10-29 11:53:19 -0500

    Change config.overrides to arrays
    ESLint 6 requires that this field is an array
    
    Closes #78

--- src/index.ts
+++ src/index.ts
@@ -247,41 +247,45 @@ export const rules = {
 export const configs = {
   recommended: {
     plugins: ['jest-formatting'],
-    overrides: {
-      files: [
-        '**/*.test.*',
-        '**/*_test.*',
-        '**/*Test.*',
-        '**/*.spec.*',
-        '**/*_spec.*',
-        '**/*Spec.*',
-        '**/__tests__/*',
-      ],
-      rules: {
-        'jest-formatting/padding-around-after-all-blocks': 2,
-        'jest-formatting/padding-around-after-each-blocks': 2,
-        'jest-formatting/padding-around-before-all-blocks': 2,
-        'jest-formatting/padding-around-before-each-blocks': 2,
-        'jest-formatting/padding-around-describe-blocks': 2,
-        'jest-formatting/padding-around-test-blocks': 2,
+    overrides: [
+      {
+        files: [
+          '**/*.test.*',
+          '**/*_test.*',
+          '**/*Test.*',
+          '**/*.spec.*',
+          '**/*_spec.*',
+          '**/*Spec.*',
+          '**/__tests__/*',
+        ],
+        rules: {
+          'jest-formatting/padding-around-after-all-blocks': 2,
+          'jest-formatting/padding-around-after-each-blocks': 2,
+          'jest-formatting/padding-around-before-all-blocks': 2,
+          'jest-formatting/padding-around-before-each-blocks': 2,
+          'jest-formatting/padding-around-describe-blocks': 2,
+          'jest-formatting/padding-around-test-blocks': 2,
+        },
       },
-    },
+    ],
   },
   strict: {
     plugins: ['jest-formatting'],
-    overrides: {
-      files: [
-        '**/*.test.*',
-        '**/*_test.*',
-        '**/*Test.*',
-        '**/*.spec.*',
-        '**/*_spec.*',
-        '**/*Spec.*',
-        '**/__tests__/*',
-      ],
-      rules: {
-        'jest-formatting/padding-around-all': 2,
+    overrides: [
+      {
+        files: [
+          '**/*.test.*',
+          '**/*_test.*',
+          '**/*Test.*',
+          '**/*.spec.*',
+          '**/*_spec.*',
+          '**/*Spec.*',
+          '**/__tests__/*',
+        ],
+        rules: {
+          'jest-formatting/padding-around-all': 2,
+        },
       },
-    },
+    ],
   },
 };
